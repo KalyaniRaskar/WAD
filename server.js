c04d563f8772bf2df2b3f54795f9d2b56f96b70d
KalyaniRaskar <raskarkalyani01@gmail.com>
2025-05-04 20:12:02 +0530

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'); const app = express();
-const port = 3000; 
-// Serve static files app.use(express.static('public'));
-
-app.listen(port, () => {
-console.log(`Server running at http://localhost:${port}`)
-});
